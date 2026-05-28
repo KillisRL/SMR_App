e881d8dc86fabe1f936f571ac36710b021da8ffe
Uéler <uelerccb1698@gmail.com>
2026-05-27 22:10:32 -0300

    endpoint de criação de recompensa

---
+++ SMRApi/Controllers/RecompensaController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SMRApi.Services;
+using SMRDominio.ClassePessoa;
+using SMRDominio.ClasseRecompensa;
+using SMRDominio.DTOs;
+using SMRInfraestrutura;
+
+namespace SMRApi.Controllers
+{
+    [ApiController]
+    [Route ("recompensas")]
+    public class RecompensaController : Controller
+    {
+        SMRDBContext _dbContext;
+
+        public RecompensaController(SMRDBContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+
+        [HttpPost("cadastrar")]
+        [Authorize]
+        public async Task<IActionResult> CadastrarRecompensa([FromBody] Recompensa recompensa)
+        {
+            if (recompensa == null || !ModelState.IsValid)
+            {
+                return BadRequest(new { Message = "Dados da recompensa inválidos." });
+            }
+
+            try
+            {
+
+                //var usuarioClaim = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value
+                //                   ?? User.FindFirst("id")?.Value;
+
+                //if (string.IsNullOrEmpty(usuarioClaim) || !int.TryParse(usuarioClaim, out int codigoUsuarioLogado))
+                //{
+                //    return Unauthorized(new { Message = "Usuário não autenticado ou identificador inválido no token." });
+                //}
+                var novaRecompensa = new Recompensa
+                {
+                    id_empresa =  recompensa.id_empresa,//codigoUsuarioLogado,
+                    titulo = recompensa.titulo,
+                    descricao = recompensa.descricao,
+                    pontos_necessarios = recompensa.pontos_necessarios,
+                    Ativo = recompensa.Ativo
+                };
+
+                _dbContext.Recompensas.Add(novaRecompensa);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(new { Message = "Recompensa cadastrada com sucesso!", Id = novaRecompensa.id });
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = "Erro ao cadastrar a recompensa", Detalhe = ex.InnerException?.Message ?? ex.Message });
+            }
+        }
+    }
+}
---
+++ SMRDominio/ClasseRecompensa/Recompensa.cs
@@ -0,0 +1,30 @@
+﻿using SMRDominio.ClassePessoa;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SMRDominio.ClasseRecompensa
+{
+    [Table("recompensa")]
+    public class Recompensa
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public int id_empresa { get; set; }
+
+        public string? titulo { get; set; }
+        public string? descricao { get; set; }
+        public int pontos_necessarios { get; set; }
+        public bool Ativo { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey("id_empresa")] 
+        public Empresa? Empresa { get; set; }
+    }
+}
