7155e5a8cfadbacfaa92eec2c16e1aa7bb625e81
UelerBernardo <uelerccb1698@gmail.com>
2026-09-09 18:47:48 -0300

    Atualização do processo de resgate de recompensa
    Atualização do Processo de Resgate de Recompensas no SMRApp

---
+++ SMRDominio/ClasseRecompensa/RecompensaResgate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMRDominio.ClasseRecompensa
+{
+    [Table("recompensa_resgate")]
+    public class RecompensaResgate
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id_recompensa_resgate { get; set; }
+        [ForeignKey("id_promotor")]
+        public int? id_promotor { get; set; }
+        [ForeignKey("id_empresa")]
+        public int id_empresa { get; set; }
+        [ForeignKey("id_recompensa")]
+        public int id_recompensa { get; set; }
+        public DateTime data_resgate { get; set; }
+    }
+}
